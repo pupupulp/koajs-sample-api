1f540dfed6fddc566a0800b53e177c4644829f28
Eagan Martin <mece.martinece@gmail.com>
2019-02-26 19:27:51 +0800

    Added Koa Router to app

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const Koa = require('koa');
-const app = new Koa(); 
-app.use(async ctx => { ctx.body = 'Hello World'; }); app.listen(3000);
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+router.get('/', (ctx, next) => {
+ ctx.body = 'Hello World!';
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
