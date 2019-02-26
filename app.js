91646f84c8f0ee62ea15f85427d3cf446b6832e2
Eagan Martin <mece.martinece@gmail.com>
2019-02-26 19:29:52 +0800

    Added logger on app

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const Logger = require('koa-logger');
+
 const app = new Koa();
 const router = new Router();
+
+app.use(Logger());
+
 router.get('/', (ctx, next) => {
  ctx.body = 'Hello World!';
 });
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(3000);
